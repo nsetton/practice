4e936921788a933c5ed1fb84d793a5cbae33b914
Nishesh Sharma <nsharma9285@gmail.com>
2016-01-24 13:50:58 -0600

    practice
    practice file

---
+++ practice.py
@@ -0,0 +1,9 @@
+def tswift_albums(song):
+    if song in {"Fearless", "Love Story", "You Belong With Me", "You're Not Sorry", "White Horse"}:
+        print("Fearless")
+    else:
+        print("1989")
+
+tswift_albums("Love Story")
+tswift_albums("Style")
+tswift_albums(2)
